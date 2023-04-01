fb2575a0699a3cb13eabb7fa1998e7382e2382db
KimTaeO <taeo0328@gmail.com>
2023-04-01 16:56:56 +0900

    :sparkles: findAllByStore 작성

--- src/main/kotlin/com/grapefruit/aid/domain/seat/repository/SeatRepository.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/seat/repository/SeatRepository.kt
@@ -1,7 +1,9 @@
 package com.grapefruit.aid.domain.seat.repository
 
 import com.grapefruit.aid.domain.seat.entity.Seat
+import com.grapefruit.aid.domain.store.entity.Store
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SeatRepository: JpaRepository<Seat, Long> {
+    fun findAllByStore(store: Store): List<Seat>
 }
