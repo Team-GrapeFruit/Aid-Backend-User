389c8382ccdf0e448845e3bca9f44a54c3395a7a
KimTaeO <taeo0328@gmail.com>
2023-04-03 21:18:38 +0900

    :sparkles: 가게, 메뉴의 제목과 설명에 글자 수 제한

--- src/main/kotlin/com/grapefruit/aid/domain/store/entity/Store.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/store/entity/Store.kt
@@ -3,13 +3,16 @@ package com.grapefruit.aid.domain.store.entity
 import com.grapefruit.aid.global.entity.BaseIdEntity
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.validation.constraints.Size
 
 
 @Entity
 class Store (
     @Column(name = "store_name", nullable = false)
+    @field:Size(max = 20)
     val storeName: String,
     @Column(nullable = false)
+    @field:Size(max = 80)
     val information: String,
     @Column(name = "store_img_url", nullable = true)
     val storeImgURL: String? = null
