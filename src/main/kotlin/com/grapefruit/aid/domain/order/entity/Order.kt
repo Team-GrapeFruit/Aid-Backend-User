f0ca6a52941c5005bce08fca982e7268eafbdc05
KimTaeO <taeo0328@gmail.com>
2023-04-06 02:49:22 +0900

    :sparkles: JoinColumn으로 테이블들 매핑

--- src/main/kotlin/com/grapefruit/aid/domain/order/entity/Order.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/order/entity/Order.kt
@@ -2,7 +2,6 @@ package com.grapefruit.aid.domain.order.entity
 
 import com.grapefruit.aid.domain.menu.entity.Menu
 import com.grapefruit.aid.domain.order.presentation.dto.MenuInfoDto
-import com.grapefruit.aid.domain.order.presentation.dto.request.CreateOrderReqDto
 import com.grapefruit.aid.domain.seat.entity.Seat
 import com.grapefruit.aid.domain.store.entity.Store
 import com.grapefruit.aid.global.entity.BaseIdEntity
@@ -13,13 +12,13 @@ class Order(
     @Column(name = "quantity", nullable = false)
     val quantity: Long,
     @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    @Column(name = "seat_id", nullable = false)
+    @JoinColumn(name = "seat_id", nullable = false)
     val seat: Seat,
     @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    @Column(name = "menu_id", nullable = false)
+    @JoinColumn(name = "menu_id", nullable = false)
     val menu: Menu,
     @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    @Column(name = "store_id", nullable = true)
+    @JoinColumn(name = "store_id", nullable = false)
     val store: Store
 ): BaseIdEntity() {
     constructor(store: Store, seat: Seat, menuInfoDto: MenuInfoDto): this(
