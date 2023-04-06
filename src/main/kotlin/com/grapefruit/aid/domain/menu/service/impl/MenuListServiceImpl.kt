83ced1fb48dc14e388c4d995e525d906988e2cb8
KimTaeO <taeo0328@gmail.com>
2023-04-06 20:06:00 +0900

    :sparkles: 메뉴 리스트 서비스 어노테이션 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/service/impl/MenuListServiceImpl.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/service/impl/MenuListServiceImpl.kt
@@ -4,12 +4,15 @@ import com.grapefruit.aid.domain.menu.entity.Menu
 import com.grapefruit.aid.domain.menu.presentation.dto.response.SingleMenuListResDto
 import com.grapefruit.aid.domain.menu.repository.MenuRepository
 import com.grapefruit.aid.domain.menu.service.MenuListService
-import com.grapefruit.aid.domain.seat.entity.Seat
 import com.grapefruit.aid.domain.store.entity.Store
 import com.grapefruit.aid.domain.store.exception.StoreNotFoundException
 import com.grapefruit.aid.domain.store.repository.StoreRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional(rollbackFor = [Exception::class])
 class MenuListServiceImpl(
     private val storeRepository: StoreRepository,
     private val menuRepository: MenuRepository
