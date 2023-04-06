6debf9603386b03e839c6beddcbadc7bac6ab6ad
KimTaeO <taeo0328@gmail.com>
2023-04-06 19:49:40 +0900

    :sparkles: MenuRepository에 쿼리 함수 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/repository/MenuRepository.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/repository/MenuRepository.kt
@@ -1,7 +1,9 @@
 package com.grapefruit.aid.domain.menu.repository
 
 import com.grapefruit.aid.domain.menu.entity.Menu
+import com.grapefruit.aid.domain.store.entity.Store
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MenuRepository: JpaRepository<Menu, Long> {
+    fun findAllByStore(store: Store): List<Menu>
 }
