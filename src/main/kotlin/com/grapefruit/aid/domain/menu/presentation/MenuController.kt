39d6f52345bbb0c86753581ff857db425820a1d8
KimTaeO <taeo0328@gmail.com>
2023-04-06 20:10:52 +0900

    :sparkles: 메뉴 리스트 api 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
@@ -1,4 +1,21 @@
 package com.grapefruit.aid.domain.menu.presentation
 
-class MenuController {
+import com.grapefruit.aid.domain.menu.presentation.dto.response.SingleMenuListResDto
+import com.grapefruit.aid.domain.menu.service.MenuListService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/menu")
+class MenuController(
+    private val menuListService: MenuListService
+) {
+    @GetMapping("/{store_id}")
+    fun getMenuList(@PathVariable("store_id") storeId: Long): ResponseEntity<List<SingleMenuListResDto>> {
+        val result: List<SingleMenuListResDto> = menuListService.execute(storeId)
+        return ResponseEntity.ok(result)
+    }
 }
