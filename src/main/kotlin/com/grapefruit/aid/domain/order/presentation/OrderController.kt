a168ae0e5e57c523c5f9099bcc8d70809e140eca
KimTaeO <taeo0328@gmail.com>
2023-04-06 18:49:26 +0900

    :sparkles: 주문 삭제 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/order/presentation/OrderController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/order/presentation/OrderController.kt
@@ -1,9 +1,13 @@
 package com.grapefruit.aid.domain.order.presentation
 
+import com.grapefruit.aid.domain.order.entity.Order
 import com.grapefruit.aid.domain.order.presentation.dto.request.CreateOrderReqDto
 import com.grapefruit.aid.domain.order.service.CreateOrderService
+import com.grapefruit.aid.domain.order.service.DeleteOrderService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,12 +18,19 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/order")
 class OrderController(
-    private val createOrderService: CreateOrderService
+    private val createOrderService: CreateOrderService,
+    private val deleteOrderService: DeleteOrderService
 ) {
-    @PostMapping
-    fun createOrder(@RequestParam seatId: Long,
+    @PostMapping("/{seat_id}")
+    fun createOrder(@PathVariable("seat_id") seatId: Long,
                     @RequestBody @Valid createOrderReqDto: CreateOrderReqDto): ResponseEntity<Void> {
         createOrderService.execute(seatId, createOrderReqDto)
         return ResponseEntity(HttpStatus.CREATED)
+    }
+
+    @DeleteMapping("/{seat_id}")
+    fun deleteOrder(@PathVariable("seat_id") seatId: Long): ResponseEntity<Void> {
+        deleteOrderService.execute(seatId)
+        return ResponseEntity.noContent().build()
     }
 }
