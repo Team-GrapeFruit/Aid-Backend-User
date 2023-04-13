7c58e3932e35f54fd7ff7e73e630f745d78b76e4
KimTaeO <taeo0328@gmail.com>
2023-04-13 23:30:35 +0900

    :sparkles: 주문 리스트 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/purchase/presentation/PurchaseController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/purchase/presentation/PurchaseController.kt
@@ -1,9 +1,11 @@
 package com.grapefruit.aid.domain.purchase.presentation
 
 import com.grapefruit.aid.domain.purchase.presentation.dto.request.CreatePurchaseReqDto
+import com.grapefruit.aid.domain.purchase.presentation.dto.response.GetPurchaseResDto
 import com.grapefruit.aid.domain.purchase.service.CreatePurchaseService
 import com.grapefruit.aid.domain.purchase.service.DeleteMenuFromPurchaseService
 import com.grapefruit.aid.domain.purchase.service.DeletePurchaseService
+import com.grapefruit.aid.domain.purchase.service.GetPurchaseListService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -14,7 +16,8 @@ import javax.validation.Valid
 class PurchaseController(
     private val createPurchaseService: CreatePurchaseService,
     private val deletePurchaseService: DeletePurchaseService,
-    private val deleteMenuFromPurchaseService: DeleteMenuFromPurchaseService
+    private val deleteMenuFromPurchaseService: DeleteMenuFromPurchaseService,
+    private val getPurchaseListService: GetPurchaseListService
 ) {
     @PostMapping("/{seat_id}")
     fun createOrder(@PathVariable("seat_id") seatId: Long,
@@ -33,5 +36,11 @@ class PurchaseController(
     fun deleteMenuFromPurchase(@PathVariable("purchase_id") purchaseId: Long): ResponseEntity<Void> {
         deleteMenuFromPurchaseService.execute(purchaseId)
         return ResponseEntity.noContent().build()
+    }
+
+    @GetMapping("/{seat_id}")
+    fun getPurchaseList(@PathVariable("seat_id") seatId: Long): ResponseEntity<List<GetPurchaseResDto>> {
+        val result = getPurchaseListService.execute(seatId)
+        return ResponseEntity.ok(result)
     }
 }
