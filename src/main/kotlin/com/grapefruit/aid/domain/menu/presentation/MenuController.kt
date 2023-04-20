0a32388438a490142361c6bc9b304888c5311090
KimTaeO <taeo0328@gmail.com>
2023-04-20 15:53:16 +0900

    :sparkles: 카테고리로 검색 컨트롤러 작성

--- src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
@@ -1,6 +1,8 @@
 package com.grapefruit.aid.domain.menu.presentation
 
+import com.grapefruit.aid.domain.menu.presentation.dto.response.GetMenuByCategoryResDto
 import com.grapefruit.aid.domain.menu.presentation.dto.response.GetMenuListResDto
+import com.grapefruit.aid.domain.menu.service.GetMenuByCategoryService
 import com.grapefruit.aid.domain.menu.service.GetMenuListService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,11 +13,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/menu")
 class MenuController(
-    private val getMenuListService: GetMenuListService
+    private val getMenuListService: GetMenuListService,
+    private val getMenuByCategoryService: GetMenuByCategoryService
 ) {
     @GetMapping("/{store_id}")
     fun getMenuList(@PathVariable("store_id") storeId: Long): ResponseEntity<List<GetMenuListResDto>> {
         val result: List<GetMenuListResDto> = getMenuListService.execute(storeId)
+        return ResponseEntity.ok(result)
+    }
+
+    @GetMapping("/category/{category_id}")
+    fun getMenuByCategory(@PathVariable("category_id") categoryId: Long): ResponseEntity<List<GetMenuByCategoryResDto>> {
+        val result: List<GetMenuByCategoryResDto> = getMenuByCategoryService.execute(categoryId)
         return ResponseEntity.ok(result)
     }
 }
