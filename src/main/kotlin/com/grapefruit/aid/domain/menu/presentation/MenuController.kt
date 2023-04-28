adf22a0a7697b9a68b55da79ac5995c71bc401cf
KimTaeO <taeo0328@gmail.com>
2023-04-28 23:04:32 +0900

    :sparkles: :: 메뉴 상세보기 컨트롤러 추가

--- src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/menu/presentation/MenuController.kt
@@ -1,8 +1,10 @@
 package com.grapefruit.aid.domain.menu.presentation
 
 import com.grapefruit.aid.domain.menu.presentation.dto.response.GetMenuByCategoryResDto
+import com.grapefruit.aid.domain.menu.presentation.dto.response.GetMenuDetailResDto
 import com.grapefruit.aid.domain.menu.presentation.dto.response.GetMenuListResDto
 import com.grapefruit.aid.domain.menu.service.GetMenuByCategoryService
+import com.grapefruit.aid.domain.menu.service.GetMenuDetailService
 import com.grapefruit.aid.domain.menu.service.GetMenuListService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/menu")
 class MenuController(
     private val getMenuListService: GetMenuListService,
-    private val getMenuByCategoryService: GetMenuByCategoryService
+    private val getMenuByCategoryService: GetMenuByCategoryService,
+    private val getMenuDetailService: GetMenuDetailService
 ) {
     @GetMapping("/{store_id}")
     fun getMenuList(@PathVariable("store_id") storeId: Long): ResponseEntity<List<GetMenuListResDto>> {
@@ -26,5 +29,12 @@ class MenuController(
     fun getMenuByCategory(@PathVariable("category_id") categoryId: Long): ResponseEntity<List<GetMenuByCategoryResDto>> {
         val result: List<GetMenuByCategoryResDto> = getMenuByCategoryService.execute(categoryId)
         return ResponseEntity.ok(result)
+    }
+
+    @GetMapping("/detail/{menu_id}")
+    fun getMenuDetail(@PathVariable("menu_id") menuId: Long): ResponseEntity<GetMenuDetailResDto> {
+        val result: GetMenuDetailResDto = getMenuDetailService.execute(menuId)
+        return ResponseEntity.ok(result)
+
     }
 }
