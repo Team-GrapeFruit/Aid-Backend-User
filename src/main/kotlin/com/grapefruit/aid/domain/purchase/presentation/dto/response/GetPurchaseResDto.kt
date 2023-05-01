8fab1588e9f2e61c1121cd47ddddb9426729af27
KimTaeO <taeo0328@gmail.com>
2023-05-01 20:59:13 +0900

    :bug: ::response description 필드 삭제

--- src/main/kotlin/com/grapefruit/aid/domain/purchase/presentation/dto/response/GetPurchaseResDto.kt
+++ src/main/kotlin/com/grapefruit/aid/domain/purchase/presentation/dto/response/GetPurchaseResDto.kt
@@ -12,14 +12,12 @@ data class GetPurchaseResDto(
         val menuId: Long,
         val menuName: String,
         val cost: Long,
-        val description: String,
         val menuImgUrl: String?
     ) {
         constructor(menu: Menu): this(
             menuId = menu.id,
             menuName = menu.menuName,
             cost = menu.cost,
-            description = menu.description,
             menuImgUrl = menu.menuImgURL
         )
     }
